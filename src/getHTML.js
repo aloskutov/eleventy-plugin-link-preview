f6bfbcdc830b321111b426d13bd3e5d89a9a4702
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-24 20:08:20 +0700

    fixed

--- src/getHTML.js
+++ src/getHTML.js
@@ -6,13 +6,15 @@ const getImage = (image) => {
 
 const getHtml = (data) => {
   return `<div class="link-preview">
-  <a class="link-preview__link"href='${data.url}'>
-    <div class="link-review__content">
-    <div class="link-preview__title">${data.title}</div>
-      <div class="link-preview__description">${data.description}</div>
-      <div class="link-preview__domain">${data.domain}</div>
+  <a class="link-preview__link" href='${data.url}'>
+    <div class="link-preview__wrapper">
+      <div class="link-preview__content">
+        <div class="link-preview__title">${data.title}</div>
+        <div class="link-preview__description">${data.description}</div>
+        <div class="link-preview__domain">${data.domain}</div>
+      </div>
+      <div class="link-preview__image"${getImage(data.image)}></div>
     </div>
-    <div class="link-preview__image"${getImage(data.image)}></div>
   </a>
 </div>`;
 };
