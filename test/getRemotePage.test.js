c6fa64c6fb88ae617ab9862adcd3b4cebcb2758b
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-25 00:44:00 +0700

    updated test

--- test/getRemotePage.test.js
+++ test/getRemotePage.test.js
@@ -1,14 +1,53 @@
 'use strict';
-
+const axios = require('axios');
 const getRemotePage = require('../src/getRemotePage');
 
-jest.mock('axios', () => {
-  return {
-    get: () => Promise.resolve({data: 'some html data'}),
-  };
+jest.mock('axios');
+
+beforeEach(()=>{
+  jest.clearAllMocks();
 });
 
 test('Test with mock #1', async () => {
-  const data = await getRemotePage('https://www.example.com');
+  const res = {data: 'some html data'};
+  axios.get.mockImplementationOnce(() => Promise.resolve(res));
+
+  const data = await getRemotePage('https://www.example.com/');
   expect(data).toBe('some html data');
+});
+
+test('Test with mock #2 Error response', async () => {
+  const errorMessage = 'Network Error';
+  const errorParams = {
+    'cause': errorMessage,
+    'response': {
+      'status': 404,
+      'statusText': 'Page not found!',
+      'headers': 'not ok!',
+    },
+  };
+  axios.get.mockImplementationOnce(() => Promise.
+      reject(new Error(errorMessage, errorParams)));
+
+  const data = await getRemotePage('https://www.example.com/error1');
+  expect(data).toBeFalsy();
+});
+
+test('Test with mock #3 Error request', async () => {
+  const errorMessage = 'Network Error';
+  const errorParams = {'request': 'Bad request!', 'cause': errorMessage};
+  axios.get.mockImplementationOnce(() => Promise.
+      reject(new Error(errorMessage, errorParams)));
+
+  const data = await getRemotePage('https://www.example.com/error2');
+  expect(data).toBeFalsy();
+});
+
+test('Test with mock #4 Error message', async () => {
+  const errorMessage = 'Network Error';
+  axios.get.mockImplementationOnce(() => Promise.
+      reject(new Error(errorMessage)));
+
+  const data = await getRemotePage('https://www.example.com/error3');
+  expect(data).toBeFalsy();
 });
