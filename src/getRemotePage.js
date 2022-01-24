e8bed979ee88d331379886a372c1f80bf359fa9e
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-25 00:44:29 +0700

    fixed

--- src/getRemotePage.js
+++ src/getRemotePage.js
@@ -8,20 +8,11 @@ const axios = require('axios');
  * @return {string}
  */
 const getRemotePage = async (url) => {
-  return axios.get(url, {'withCredentials': true}).
+  return axios.get(url).
       then((response) => response.data).
-      catch(function(error) {
-        if (error.response) {
-          // Request made and server responded
-          console.log('Status: ', error.response.status);
-          console.log('Status text: ', error.response.statusText);
-          console.log('Header: ', error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.log(error.request);
-        } else {
-          console.log('Error', error.message);
-        }
+      catch((error) => {
+        console.log('Requested URL: ', url);
+        console.log('Error: ', error.message);
         return false;
       });
 };
