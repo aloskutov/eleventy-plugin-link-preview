d62fd0f020e5cf07eaef76724c4f751b231f0986
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-24 20:18:39 +0700

    updated

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const getRemotePage = require('./src/getRemotePage');
 const getPageData = require('./src/getPageData');
 const getHtml = require('./src/getHtml');
 
-const url = 'https://play.tailwindcss.com/';
+const url = 'https://dev.to/';
 
 getRemotePage(url).then((content) => {
-  const data = getPageData(content, url);
-  if (data != {}) {
-    console.log(getHtml(data));
-  }
+  const data = content ? getPageData(content, url) : false;
+  const result = (data && data !== {}) ? getHtml(data) : '';
+  console.log(result);
 });
