2128f449a2c455e237ddf6ed92521c35f3bf2310
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-24 20:18:21 +0700

    catching errors

--- src/getRemotePage.js
+++ src/getRemotePage.js
@@ -8,7 +8,22 @@ const axios = require('axios');
  * @return {string}
  */
 const getRemotePage = async (url) => {
-  return axios.get(url).then((response) => response.data);
+  return axios.get(url, {'withCredentials': true}).
+      then((response) => response.data).
+      catch(function(error) {
+        if (error.response) {
+          // Request made and server responded
+          console.log('Status: ', error.response.status);
+          console.log('Status text: ', error.response.statusText);
+          console.log('Header: ', error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+        return false;
+      });
 };
 
 module.exports = getRemotePage;
