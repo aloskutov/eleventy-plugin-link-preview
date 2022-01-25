8bc9134eaa3982cdb2081d91aedc527773959d56
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-25 14:58:03 +0700

    refactored

--- src/getHTML.js
+++ src/getHTML.js
@@ -1,17 +1,63 @@
 'use strict';
 
+/**
+ * Get url string
+ * @param {string} url
+ * @return {string}
+ */
+const getUrl = (url) => {
+  return url ? url : '';
+};
+
+/**
+ * Get title string
+ * @param {string} title
+ * @return {string}
+ */
+const getTitle = (title) => {
+  return title ? title : '';
+};
+
+/**
+ * Get description string
+ * @param {string} description
+ * @return {string}
+ */
+const getDescription = (description) => {
+  return description ? description : '';
+};
+
+/**
+ * Get domain string
+ * @param {string} domain
+ * @return {string}
+ */
+const getDomain = (domain) => {
+  return domain ? domain : '';
+};
+
+/**
+ * Get image string
+ * @param {string} image
+ * @return {string}
+ */
 const getImage = (image) => {
   return image ? ` style='background-image: url(${image});'` : '';
 };
 
-const getHtml = (data) => {
+/**
+ * Get html code
+ * @param {object} data
+ * @return {string}
+ */
+const getHtml = (data = {}) => {
   return `<div class="link-preview">
-  <a class="link-preview__link" href='${data.url}'>
+  <a class="link-preview__link" href='${getUrl(data.url)}'>
     <div class="link-preview__wrapper">
       <div class="link-preview__content">
-        <div class="link-preview__title">${data.title}</div>
-        <div class="link-preview__description">${data.description}</div>
-        <div class="link-preview__domain">${data.domain}</div>
+        <div class="link-preview__title">${getTitle(data.title)}</div>
+        <div class="link-preview__description">${getDescription(data.description)}</div>
+        <div class="link-preview__domain">${getDomain(data.domain)}</div>
       </div>
       <div class="link-preview__image"${getImage(data.image)}></div>
     </div>
