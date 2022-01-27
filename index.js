2ff5145ec42963346fef18021a496f309d33ac0e
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-27 13:57:30 +0700

    fixed: JS-0111

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const getStyle = require('./src/getStyle');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addShortcode('linkPreview', async (url) => {
-    return await linkPreview(url);
+    const result = await linkPreview(url);
+    return result;
   });
   eleventyConfig.addShortcode('linkPreviewCss', () => {
     return getStyle();
