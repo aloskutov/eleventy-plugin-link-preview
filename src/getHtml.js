8efb1b454c530e1b1f3f156db98e7707c092e136
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-29 21:25:48 +0700

    fixed

--- src/getHtml.js
+++ src/getHtml.js
@@ -5,35 +5,35 @@
  * @param {string} url
  * @return {string}
  */
-const getUrl = (url) => url ? url : '';
+const getUrl = (url) => (url ? url : '');
 
 /**
  * Get title string
  * @param {string} title
  * @return {string}
  */
-const getTitle = (title) => title ? title : '';
+const getTitle = (title) => (title ? title : '');
 
 /**
  * Get description string
  * @param {string} description
  * @return {string}
  */
-const getDescription = (description) => description ? description : '';
+const getDescription = (description) => (description ? description : '');
 
 /**
  * Get domain string
  * @param {string} domain
  * @return {string}
  */
-const getDomain = (domain) => domain ? domain : '';
+const getDomain = (domain) => (domain ? domain : '');
 
 /**
  * Get image string
  * @param {string} image
  * @return {string}
  */
-const getImage = (image) => image ? `<div class="link-preview__image" style="background-image: url(${image});"></div>` : '';
+const getImage = (image) => (image ? `<div class="link-preview__image" style="background-image: url(${image});"></div>` : '');
 
 /**
  * Get html code
