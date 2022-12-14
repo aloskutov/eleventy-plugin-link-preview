193eed2a325ad6bf063ee54ac76aa588c70bd28a
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-12-15 00:52:05 +0700

    added nunjucks shortcode

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const linkPreview = require('./src/linkPreview');
 const getStyle = require('./src/getStyle');
 
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addNunjucksAsyncShortcode('linkPreview', async (url) => {
+    const result = await linkPreview(url);
+    return result;
+  });
   eleventyConfig.addLiquidShortcode('linkPreview', async (url) => {
     const result = await linkPreview(url);
     return result;
